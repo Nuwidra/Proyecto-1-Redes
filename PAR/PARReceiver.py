b89ad11a1fb858b4493228b7eed8c7da438387f0
rcruz_23 <rcruz89@gmail.com>
2023-04-10 01:28:14 -0600

    Par receiver

---
+++ PAR/PARReceiver.py
@@ -0,0 +1,29 @@
+import socket
+import pickle
+
+RECEIVER_ADDR = ('localhost', 8025)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(RECEIVER_ADDR)
+
+expected_seq_no = 0
+
+while True:
+    pkt, addr = sock.recvfrom(1024)
+    packet = pickle.loads(pkt)
+    seq_no = packet[0]
+    data = packet[1]
+
+    if seq_no == expected_seq_no:
+        # received expected packet
+        print(f"Received: {data}")
+        # send ACK
+        ack_packet = [expected_seq_no]
+        sock.sendto(pickle.dumps(ack_packet), ('localhost', 8000))
+        # increment expected sequence number
+        expected_seq_no = (expected_seq_no + 1) % 2
+    else:
+        # received out-of-order packet, discard and wait for retransmission
+        print(f"Discarding out-of-order packet with seq_no: {seq_no}")
+
+sock.close()
---
+++ PAR/PARSender.py
@@ -0,0 +1,35 @@
+import socket
+import pickle
+import time
+
+RECEIVER_ADDR = ('localhost', 8025)
+SENDER_ADDR = ('localhost', 8000)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(SENDER_ADDR)
+
+def send_data():
+    # get input from user
+    num_frames = int(input("Enter the number of frames to send: "))
+    data = input("Enter the data to send: ")
+
+    seq_no = 0
+    for i in range(num_frames):
+        # create packet with sequence number and data
+        packet = [seq_no, data]
+        # send packet
+        sock.sendto(pickle.dumps(packet), RECEIVER_ADDR)
+        print(f"Sent packet with seqNo={seq_no}")
+        # wait for ACK
+        while True:
+            ack_packet, addr = sock.recvfrom(1024)
+            ack = pickle.loads(ack_packet)
+            ack_seq_no = ack[0]
+            if ack_seq_no == seq_no:
+                print(f"Received ACK with seqNo={ack_seq_no}")
+                break
+        # increment sequence number
+        seq_no = (seq_no + 1) % 2
+
+send_data()
+sock.close()
