acace73b35bff6ef366fd13dda148b782e5abad1
rcruz_23 <rcruz89@gmail.com>
2023-04-13 01:50:02 -0600

    par update

--- PAR/PARReceiver.py
+++ PAR/PARReceiver.py
@@ -1,6 +1,7 @@
 import socket
 import pickle
 from events.events import *
+from frame.frame import Frame, Packet
 
 RECEIVER_ADDR = ('localhost', 8025)
 
@@ -10,23 +11,29 @@ sock.bind(RECEIVER_ADDR)
 expected_seq_no = 0
 
 while True:
-    pkt, addr = sock.recvfrom(1024)
-    packet = pickle.loads(pkt)
-    seq_no = packet[0]
-    data = packet[1]
+    # receive frame over socket and unpickle it
+    frame_packet, addr = sock.recvfrom(1024)
+    frame = pickle.loads(frame_packet)
 
-    if seq_no == expected_seq_no:
-        from_physical_layer(packet)
-        # received expected packet
-        print(f"Received: {data}")
+    # check if it's the end of the transmission
+    if frame.data == "":
+        break
+
+    # check if the sequence number matches the expected value
+    if frame.sequence_number == expected_seq_no:
+        # send packet to the network layer
+        from_physical_layer(frame.data)
+        print(f"Received: {frame.data}")
         # send ACK
-        ack_packet = [expected_seq_no]
-        sock.sendto(pickle.dumps(ack_packet), ('localhost', 8000))
+        ack = Frame(Packet(""))
+        ack.type = "ack"
+        ack.sequence_number = expected_seq_no
+        ack.confirmation_number = expected_seq_no
+        sock.sendto(pickle.dumps(ack), ('localhost', 8000))
         # increment expected sequence number
         expected_seq_no = (expected_seq_no + 1) % 2
-        to_physical_layer(packet)
     else:
-        # received out-of-order packet, discard and wait for retransmission
-        print(f"Discarding out-of-order packet with seq_no: {seq_no}")
+        # received out-of-order frame, discard and wait for retransmission
+        print(f"Discarding out-of-order frame with seqNo: {frame.sequence_number}")
 
 sock.close()
--- PAR/PARSender.py
+++ PAR/PARSender.py
@@ -2,6 +2,7 @@ import socket
 import pickle
 import timer.timer
 from events.events import *
+from frame.frame import Frame, Packet
 
 RECEIVER_ADDR = ('localhost', 8025)
 SENDER_ADDR = ('localhost', 8000)
@@ -14,27 +15,44 @@ def send_data():
     num_frames = int(input("Enter the number of frames to send: "))
     data = input("Enter the data to send: ")
 
-    from_network_layer(data)
+    # create initial packet with data
+    packet = Packet(data)
+
     seq_no = 0
     for i in range(num_frames):
-        # create packet with sequence number and data
-        packet = [seq_no, data]
-        to_physical_layer(packet)
-        # send packet
-        sock.sendto(pickle.dumps(packet), RECEIVER_ADDR)
-        print(f"Sent packet with seqNo={seq_no}")
+        # create frame with packet and sequence number
+        frame = Frame(packet)
+        frame.sequence_number = seq_no
+
+        # send frame
+        sock.sendto(pickle.dumps(frame), RECEIVER_ADDR)
+        print(f"Sent frame with seqNo={seq_no}")
+
+        # start timer for frame
+        start_timer(frame.sequence_number)
+
         # wait for ACK
         while True:
-            from_physical_layer(packet)
             ack_packet, addr = sock.recvfrom(1024)
             ack = pickle.loads(ack_packet)
-            ack_seq_no = ack[0]
-            if ack_seq_no == seq_no:
-                print(f"Received ACK with seqNo={ack_seq_no}")
-                from_network_layer(packet)
+            if isinstance(ack, Frame) and ack.confirmation_number == seq_no:
+                print(f"Received ACK with seqNo={ack.confirmation_number}")
+                # stop timer for frame
+                stop_timer(seq_no)
                 break
+
         # increment sequence number
         seq_no = (seq_no + 1) % 2
+
+        # update packet sequence number
+        packet.sequence_number = seq_no
+
+    # send empty frame to signal end of transmission
+    end_frame = Frame(Packet(""))
+    end_frame.sequence_number = seq_no
+    sock.sendto(pickle.dumps(end_frame), RECEIVER_ADDR)
+
+    sock.close()
 
 send_data()
 sock.close()
