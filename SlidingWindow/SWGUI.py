2e5069b092dfe1a667a1b4675d1be5f5c5fc4bd1
rcruz_23 <rcruz89@gmail.com>
2023-04-18 16:35:54 -0600

    sw gui updates

--- SlidingWindow/SWGUI.py
+++ SlidingWindow/SWGUI.py
@@ -1,9 +1,10 @@
 from tkinter import *
+import threading
+from SlidingWindow.SWsend import send_packets, stop_sw_sender
+from SlidingWindow.SWreceive import receive_packets
+from SlidingWindow.frame import Packet
 
 # create a window
-from SlidingWindow.SWreceive import receive_packets
-from SlidingWindow.SWsend import send_packets, stop_sw_sender
-
 window = Tk()
 
 # set the title
@@ -12,52 +13,48 @@ window.title("Sliding Window Protocol")
 # set the size
 window.geometry('400x300')
 
-# create labels
-timeout_label = Label(window, text="TIMEOUT_INTERVAL:")
-timeout_label.grid(column=0, row=0)
 
-sleep_label = Label(window, text="SLEEP_INTERVAL:")
-sleep_label.grid(column=0, row=1)
+no_packets_label = Label(window, text="No of packets to be sent:")
+no_packets_label.grid(column=0, row=2)
 
-window_size_label = Label(window, text="Window size:")
-window_size_label.grid(column=0, row=2)
-
-data_label = Label(window, text="Data to be transferred:")
-data_label.grid(column=0, row=3)
+packet_data_label = Label(window, text="Packet Data:")
+packet_data_label.grid(column=0, row=3)
 
 # create entry fields
-timeout_entry = Entry(window)
-timeout_entry.grid(column=1, row=0)
+no_packets_entry = Entry(window)
+no_packets_entry.grid(column=1, row=2)
 
-sleep_entry = Entry(window)
-sleep_entry.grid(column=1, row=1)
+packet_data_entry = Entry(window)
+packet_data_entry.grid(column=1, row=3)
 
-window_size_entry = Entry(window)
-window_size_entry.grid(column=1, row=2)
-
-data_entry = Entry(window)
-data_entry.grid(column=1, row=3)
+# create function to run send in a separate thread
+def send_thread():
+    packets = []
+    sender_ip = '127.0.0.1'
+    sender_port = 5000
+    receiver_ip = '127.0.0.1'
+    receiver_port = 6000
+    for i in range(int(no_packets_entry.get())):
+        packets.append(Packet(seq_num=i, data=packet_data_entry.get().encode()))
+    send_packets(packets, receiver_ip, receiver_port)
 
 # create button to send data
-send_button = Button(window, text="Send", command=lambda: send_packets(
-    data_entry.get(),
-    int(window_size_entry.get()),
-    float(timeout_entry.get()),
-    float(sleep_entry.get())
-))
+send_button = Button(window, text="Send", command=lambda: threading.Thread(target=send_thread).start())
 send_button.grid(column=1, row=4)
 
+# create function to run receive in a separate thread
+def receive_thread():
+    receiver_ip = '127.0.0.1'
+    receiver_port = 6000
+    receive_packets(receiver_ip, receiver_port)
+
 # create button to receive data
-receive_button = Button(window, text="Receive", command=receive_packets)
+receive_button = Button(window, text="Receive", command=lambda: threading.Thread(target=receive_thread).start())
 receive_button.grid(column=1, row=5)
 
-# create button to stop sender and receiver
-stop_button = Button(window, text="Stop", command=lambda: [stop_sw_sender(), stop_sw_receiver()])
+# create button to stop sender
+stop_button = Button(window, text="Stop", command=stop_sw_sender)
 stop_button.grid(column=1, row=6)
-
-# create button to pause sending
-pause_button = Button(window, text="Pause")
-pause_button.grid(column=1, row=7)
 
 # run the window
 window.mainloop()
